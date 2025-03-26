9ad4dd20b0cbcbfc7489bf863e9f120e48154abc
loipv03 <phanvanloi1522003@gmail.com>
2025-03-26 18:06:47 +0700

    Update

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import authRouter from "./routers/auth";
 import connectDB from "./configs/database";
 import errorHandler from "./middlewares/errorMiddlware";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -11,6 +12,7 @@ const basePath: string = "/api/v1";
 const MONGODB_URI: string = process.env.MONGODB_URI as string;
 
 const app = express();
+app.use(cookieParser());
 
 app.use(express.json());
 app.use(
