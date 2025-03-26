ce3595dfa7825270ef390b8446b0dfc2eb428b72
loipv03 <phanvanloi1522003@gmail.com>
2025-03-26 18:00:56 +0700

    update

--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -7,16 +7,13 @@ const authRouter = express.Router();
 authRouter.post("/signup", authControlllers.signup);
 authRouter.post("/signin", authControlllers.signin);
 authRouter.post("/verify-otp", authControlllers.verifyOtp);
-authRouter.get(
-  "/me",
-  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    try {
-      console.log(req.cookies);
-    } catch (error) {
-      next(error);
-    }
-    res.json({ message: "okeeeeeeee" });
+authRouter.get("/me", (req: Request, res: Response, next: NextFunction) => {
+  try {
+    console.log(req.cookies);
+  } catch (error) {
+    next(error);
   }
-);
+  res.json({ message: "okeeeeeeee" });
+});
 
 export default authRouter;
